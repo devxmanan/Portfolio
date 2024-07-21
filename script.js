e64bbea2415b5ca29fbbaf0327fc86999723c665
Manan Kandpal <manank.7409@gmail.com>
2024-07-21 19:23:40 +0530

    update login system

--- script.js
+++ script.js
@@ -1,10 +1,110 @@
-let menuline1 = document.getElementById("menuline1");
-let menuline2 = document.getElementById("menuline2");
-let menuline3 = document.getElementById("menuline3");
-let menuicon = document.getElementById("menuicon");
+//FIREBASE CONFIGURATION
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDpMgubNJgQKnUXpXLdqbX8ONeKUYlSVsE",
+    authDomain: "portfolio-manan-2703.firebaseapp.com",
+    databaseURL: "https://portfolio-manan-2703-default-rtdb.firebaseio.com",
+    projectId: "portfolio-manan-2703",
+    storageBucket: "portfolio-manan-2703.appspot.com",
+    messagingSenderId: "76323952350",
+    appId: "1:76323952350:web:48d1b1ff600a072f69944f"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth()
+const database = firebase.database()
+
+auth.onAuthStateChanged(function (user) {
+    if (user) {
+        // User is signed in.
+        console.log("User signed in:", user.uid);
+        const navItem = document.getElementById("checkUser")
+        const a = document.createElement('a');
+        a.href = 'account.html';
+        a.innerText = "Account";
+        navItem.appendChild(a);
+        // Do something with the user information.
+    } else {
+        // No user is signed in.
+        console.log("User not signed in");
+        const navItem = document.getElementById("checkUser")
+        const a = document.createElement('a');
+        a.href = 'login.html';
+        a.innerText = "Login";
+        navItem.appendChild(a);
+    }
+});
+
+
+function register(event) {
+    event.preventDefault()
+    let name = document.getElementById("name").value
+    let email = document.getElementById("email").value
+    let password = document.getElementById("password").value
+    console.log(name, email, password);
+    auth.createUserWithEmailAndPassword(email, password)
+        .then(function () {
+            var user = auth.currentUser
+            var user_data = {
+                email: email,
+                name: name,
+                password: password,
+                account_created: Date.now(),
+                last_login: Date.now()
+            }
+            alert("User Created Successfully!")
+
+            const usersRef = database.ref("users");
+
+            // Save user data using set()
+            usersRef.child(user.uid).set(user_data)
+                .then(() => {
+                    alert("Data saved successfully!");
+                })
+                .catch((error) => {
+                    alert(`Error saving data: ${error.message}`)
+                });
+        })
+        .catch(function (error) {
+            alert(`Error - ${error.message} Try Logging in instead`)
+        });
+}
+
+
+function login(event) {
+    event.preventDefault()
+    let email = document.getElementById("email").value
+    let password = document.getElementById("password").value
+    auth.signInWithEmailAndPassword(email, password)
+        .then(function () {
+            var user = auth.currentUser
+            var user_data = {
+                last_login: Date.now()
+            }
+            alert("User Logged in successfully!")
+
+            const usersRef = database.ref("users");
+
+            // Save user data using set()
+            usersRef.child(user.uid).update(user_data)
+                .then(() => {
+                    alert("Data updated successfully!");
+                })
+                .catch((error) => {
+                    alert(`Error updating data: ${error.message}`)
+                });
+        })
+        .catch(function (error) {
+            alert(`Error - ${error.message}`)
+        });
+}
+
+
+//ON LOAD FUNCTION
 let currentTheme = localStorage.getItem("theme");
-window.onload = loadtheme();
-function loadtheme() {
+
+window.onload = load();
+function load() {
     document.querySelector('body').classList.add(currentTheme)
     if (currentTheme == "light") {
         document.getElementById("themeIcon").classList.remove("fa-moon-o", "fa-sun-o");
@@ -14,6 +114,14 @@ function loadtheme() {
         document.getElementById("themeIcon").classList.add("fa-sun-o")
     }
 }
+
+//HAMBURGER AND THEME
+let menuline1 = document.getElementById("menuline1");
+let menuline2 = document.getElementById("menuline2");
+let menuline3 = document.getElementById("menuline3");
+let menuicon = document.getElementById("menuicon");
+
+
 menuline1.style.transform = "none";
 let sidebar = document.getElementById("sidebar");
 function toggleMenu() {
